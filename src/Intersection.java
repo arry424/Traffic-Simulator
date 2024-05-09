bef8f94d56ca80c93a585dcf905cb8fac90783b7
arry424 <weinbergerhi@gmail.com>
2024-05-09 12:25:37 -0500

    Fixed Intersection Constructor

--- src/Intersection.java
+++ src/Intersection.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.Queue;
+import java.util.LinkedList;
 public class Intersection {
     private ArrayList[] lanes;
     private TrafficLight trafficLightHorizontal;
@@ -8,8 +9,12 @@ public class Intersection {
 
     public Intersection(){
         lanes = new ArrayList[4];
-        for(int i = 0; i < lanes.length; i++)
-            lanes[i]  = new ArrayList<Queue<Car>>();
+        for(int i = 0; i < lanes.length; i++) {
+            lanes[i] = new ArrayList<Queue<Car>>();
+            for(int j = 0; j < 2; j++){
+                lanes[i].add(new LinkedList<Car>());
+            }
+        }
         trafficLightHorizontal = new TrafficLight();
         trafficLightVertical = new TrafficLight();
     }
