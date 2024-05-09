d9e49f574c97fa668bff12bbd34d61833d90acbb
sjohnson1512 <154621049+sjohnson1512@users.noreply.github.com>
2024-05-09 18:07:04 -0500

    Car go move

--- src/IntersectionTester.java
+++ src/IntersectionTester.java
@@ -9,16 +9,7 @@ public class IntersectionTester {
     public static void main(String[] args) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
         System.out.println("Hello world!");
 
-        SimpleAudioPlayer audioPlayer = new SimpleAudioPlayer("SFX/honk.wav",true);
-        audioPlayer.play();
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-
-
-        DrawingPanel panel = new DrawingPanel(600,600 );
-
-        Car car = new Car();
-        car.draw(panel.getGraphics());
 
     }
 }
