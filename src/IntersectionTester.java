993735a512123f5ff96a9c903a029612dff784f6
sjohnson1512 <154621049+sjohnson1512@users.noreply.github.com>
2024-05-09 13:24:42 -0500

    Audio, and visual additions

--- src/IntersectionTester.java
+++ src/IntersectionTester.java
@@ -1,5 +1,10 @@
+import java.awt.*;
+
 public class IntersectionTester {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        DrawingPanel panel = new DrawingPanel((int)screenSize.getWidth()-100,(int)screenSize.getHeight()-100 );
     }
 }
