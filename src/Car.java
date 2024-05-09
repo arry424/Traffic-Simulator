998c8be3ce00880499859d8976084aceef3a0547
arry424 <weinbergerhi@gmail.com>
2024-05-09 12:47:45 -0500

    Work on Intersection

--- src/Car.java
+++ src/Car.java
@@ -1,12 +1,15 @@
 public class Car {
     private int waitTime;
+    private int happiness;
 
     public Car(){
         waitTime = 0;
+        happiness = (int)(Math.random()*100+1);
     }
 
-    public Car(int wt){
+    public Car(int wt, int hap){
         waitTime = wt;
+        happiness = hap;
     }
 
     public void setWaitTime(int wt){
@@ -15,6 +18,14 @@ public class Car {
 
     public int getWaitTime(){
         return waitTime;
+    }
+
+    public int getHappiness() {
+        return happiness;
+    }
+
+    public void setHappiness(int happiness) {
+        this.happiness = happiness;
     }
 
     //TODO Make a toString that prints something that looks nice
--- src/Intersection.java
+++ src/Intersection.java
@@ -3,10 +3,18 @@ import java.util.ArrayList;
 import java.util.Queue;
 import java.util.LinkedList;
 public class Intersection {
-    private ArrayList[] lanes;
+    private ArrayList<Queue<Car>>[] lanes;
     private TrafficLight trafficLightHorizontal;
     private TrafficLight trafficLightVertical;
 
+    /**
+     * The intersection is represented by an array of lists of queues
+     * Each spot in the array is one of the four directions, and each list
+     * holds queues that represent each lane that stores the cars
+     *
+     * 0 and 2 are the verticals (0 is top, 2 is bottom)
+     * 1 and 3 are the horizontals (1 is left, 3 is right)
+     */
     public Intersection(){
         lanes = new ArrayList[4];
         for(int i = 0; i < lanes.length; i++) {
@@ -17,5 +25,28 @@ public class Intersection {
         }
         trafficLightHorizontal = new TrafficLight();
         trafficLightVertical = new TrafficLight();
+    }
+
+    public void run(){
+        int time = 0;
+        while(time<1000){
+
+            int percentCar = (int)(Math.random()*100+1);
+            if(percentCar <= 30){
+                int dir = (int)(Math.random()*4);
+                int lane = (int)(Math.random() * lanes[dir].size());
+                lanes[dir].get(lane).add(new Car());
+            } // adds a car to a random direction and a random lane in that direction
+
+            if(trafficLightVertical.getLight() == 3){ //green
+
+            }
+
+            if(trafficLightHorizontal.getLight() == 3){//green
+
+            }
+
+            time++;
+        }
     }
 }
