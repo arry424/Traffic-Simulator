b683ff7a6bf7b69d061d3ba0cd4dfdda2c662582
sjohnson1512 <154621049+sjohnson1512@users.noreply.github.com>
2024-05-09 21:34:31 -0500

    More car

--- src/GraphicsTester.java
+++ src/GraphicsTester.java
@@ -10,25 +10,35 @@ public class GraphicsTester {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
 
-        DrawingPanel panel = new DrawingPanel(800, 600);
+        DrawingPanel panel = new DrawingPanel(800, 800);
+        panel.getGraphics().setColor(Color.green.darker().darker());
+        panel.getGraphics().fillRect(0,0,800,800);
         while(true){
-        for(int i = 600; i >0; i--) {
-            drawRoadVerticle(panel.getGraphics(), 1);
-            drawCarVeriticle(panel.getGraphics(), 422, i);
-            Thread.sleep(10);
+        for(int i = 800; i >0; i--) {
+
+            drawRoadUp(panel.getGraphics(), 2);
+            drawRoadDown(panel.getGraphics(),2);
+            drawRoadLeft(panel.getGraphics(),2);
+            drawRoadRight(panel.getGraphics(),2);
+
+            drawCarVeriticle(panel.getGraphics(), 430, i,Color.pink);
+            drawCarVeriticle(panel.getGraphics(),475,i-10,Color.CYAN.darker());
+
+
+            Thread.sleep(20);
         }
         }
 
     }
 
-    public static void drawCarVeriticle(Graphics g, int x, int y){
-        g.setColor(new Color(255,50,50));
+    public static void drawCarVeriticle(Graphics g, int x, int y, Color c){
+        g.setColor(c);
         g.fillRoundRect(x-15, y-30, 30,60, 10,10);
         g.fillRoundRect(x-20,y-10,40,10,10,10);
         g.setColor(Color.BLACK);
         g.fillRoundRect(x-13,y-17,26,13,10,10);
         g.fillRoundRect(x-13,y+15,26,12,10,10);
-        g.setColor(new Color(255,75,75));
+        g.setColor(c);
         g.fillRect(x-14,y-1,28,14);
         g.setColor(Color.ORANGE);
         g.fillRoundRect(x-15,y-30,5,5,10,10);
@@ -36,8 +46,24 @@ public class GraphicsTester {
 
     }
 
-    public static void drawRoadVerticle(Graphics g, int lanes){
-        g.setColor(Color.DARK_GRAY);
-        g.fillRect(400,0,lanes*(45),800);
+    public static void drawRoadUp(Graphics g, int lanes){
+        g.setColor(Color.GRAY);
+        g.fillRect(410,0,lanes*(45),800);
+    }
+
+    public static void drawRoadDown(Graphics g,int lanes){
+        g.setColor(Color.GRAY);
+        g.fillRect(390-(lanes*(45)),0,lanes*(45),800);
+    }
+
+    public static void drawRoadLeft(Graphics g, int lanes){
+        g.setColor(Color.GRAY);
+        g.fillRect(0,390-(lanes*(45)),800,lanes * (45));
+    }
+
+    public static void drawRoadRight(Graphics g, int lanes){
+        g.setColor(Color.GRAY);
+        g.fillRect(0,410,800,lanes*(45));
     }
 }
+
